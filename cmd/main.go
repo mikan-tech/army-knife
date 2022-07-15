d0246002afa62c0ab9203e3c6a5ac932bb66afd2
hoshitocat <hoshitocat@gmail.com>
2022-07-15 16:35:50 +0900

    :tada: first commit

---
+++ cmd/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+const authAPIEndpoint = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+
+var (
+	apiKey   string
+	email    string
+	password string
+)
+
+func init() {
+	flag.StringVar(&apiKey, "key", os.Getenv("API_KEY"), "Use signInWithPassword api key")
+	flag.StringVar(&email, "email", os.Getenv("EMAIL"), "Use signInWithPassword user's email.")
+	flag.StringVar(&password, "password", os.Getenv("PASSWORD"), "Use signInWithPassword user's password.")
+	flag.Parse()
+}
+
+func main() {
+	ctx := context.Background()
+	payload, err := signInWithPassword(ctx, &signInWithPasswordInput{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		fmt.Printf("failed signInWithPassword(). Reason %s\n", err.Error())
+		return
+	}
+
+	fmt.Printf("Bearer %s", payload.IDToken)
+}
+
+type signInWithPasswordInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type signInWithPasswordPayload struct {
+	Email   string `json:"email"`
+	IDToken string `json:"idToken"`
+}
+
+type errorPayload struct {
+	Error struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Errors  []struct {
+			Message string `json:"message"`
+			Domain  string `json:"global"`
+			Reason  string `json:"invalid"`
+		} `json:"errors"`
+	} `json:"error"`
+}
+
+func signInWithPassword(ctx context.Context, input *signInWithPasswordInput) (*signInWithPasswordPayload, error) {
+	reqBodyBytes, err := json.Marshal(input)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := bytes.NewReader(reqBodyBytes)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, authAPIEndpoint, reader)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	values := req.URL.Query()
+	values.Set("key", apiKey)
+	req.URL.RawQuery = values.Encode()
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	respBodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var errorResp errorPayload
+		err = json.Unmarshal(respBodyBytes, &errorResp)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, errors.New(errorResp.Error.Message)
+	}
+
+	var payload signInWithPasswordPayload
+	err = json.Unmarshal(respBodyBytes, &payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return &payload, nil
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mikan-tech/army-knife
+
+go 1.18
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129
+)
+
+require golang.org/x/text v0.3.7 // indirect
